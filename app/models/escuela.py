0da6bb877268cb24094b1d5d81b5bbc95dbafd8f
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2026-01-27 00:41:50 -0300

    fix: CUE ya no es opcional en el modelo Escuela

--- app/models/escuela.py
+++ app/models/escuela.py
@@ -8,5 +8,5 @@ if TYPE_CHECKING:
     from .usuario import Usuario
 
 class Escuela(EscuelaBase, table=True):
-        CUE: str | None = Field(max_length=9,min_length=9, primary_key=True)
+        CUE: str = Field(max_length=9,min_length=9, primary_key=True)
         usuarios: list["Usuario"] = Relationship(back_populates="escuelas", link_model=Rol)
